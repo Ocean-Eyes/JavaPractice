ca1e40ed66e7fe7b53a959310d5708cc1a90ea41
Ocean-Eyes <dlstlr52@naver.com>
2021-03-16 15:28:59 +0900

    Test Constructor Overloading

--- src/WildDog.java
+++ src/WildDog.java
@@ -1,19 +1,40 @@
 public class WildDog extends Dog{
+
+    // Need to make default constructor, when you overloads constructors.
+    public WildDog() { }
+    // Constructor Overloading.
+    public WildDog(int number)
+    {
+        if(number == 1)
+        {
+            this.setName("Shepherd");
+        }
+        else if(number == 2)
+        {
+            this.setName("Poodle");
+        }
+    }
+
+    // Method Overriding  => It has a same input, but functions differently
     public void sleep()
     {
         System.out.println(this.name + " zzz outside");
     }
 
+    // Method Overloading => It has a different input
     public void sleep(int hours)
     {
-        System.out.println(this.name + " zzz outside for" + hours);
+        System.out.println(this.name + " zzz outside for " + hours + "hour(s)");
     }
 
     public static void main(String[] args)
     {
         WildDog wilddog = new WildDog();
-        wilddog.setName("strong");
-        wilddog.sleep();                    // Method Overriding  => It has a same input, but functions differently
-        wilddog.sleep(3);             // Method Overloading => It has a different input
+        WildDog wildpuppy = new WildDog(2);
+        wilddog.setName("Pug");
+        System.out.println("the name of wilddog is " + wilddog.name);
+        System.out.println("the type of wildpuppy is " + wildpuppy.name);
+        wilddog.sleep();
+        wilddog.sleep(3);
     }
 }
