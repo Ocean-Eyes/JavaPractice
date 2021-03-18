d4736c6d1e0da502948afc01c27cb714bf0f7b0f
Ocean-Eyes <dlstlr52@naver.com>
2021-03-18 11:51:20 +0900

    Test Exception

---
+++ src/TestException.java
@@ -0,0 +1,31 @@
+public class TestException {
+
+    // This method should be run for test 'finally'
+    public void ShouldBeRun()
+    {
+        System.out.println("ShouldBeRun");
+    }
+
+    public static void main(String[] args)
+    {
+        TestException te = new TestException();
+        int c = 0;
+
+        // You can write codes you want to try, which may cause an exception.
+        try
+        {
+            c = 4/0;
+        }
+        // This grammar helps running codes that suggest another codes when an exception happens.
+        catch(ArithmeticException e)
+        {
+            c = -1;
+        }
+        // This grammar helps running codes that should be run even if an exception happens.
+        finally
+        {
+            System.out.println(c);
+            te.ShouldBeRun();
+        }
+    }
+}
